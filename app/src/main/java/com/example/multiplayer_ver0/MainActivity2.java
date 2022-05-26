c2e24e9700d288a28eeb4a6990f55ffb0e4cc502
AgnieszkaStaniszewska <agnieszka.czypryna@wp.pl>
2022-05-26 13:57:10 +0200

    Multiplayer game

---
+++ app/src/main/java/com/example/multiplayer_ver0/MainActivity2.java
@@ -0,0 +1,130 @@
+package com.example.multiplayer_ver0;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    ListView listView;
+    Button button;
+
+    List<String> roomsList;
+
+    String playerName = "";
+    String roomName = "";
+
+    FirebaseDatabase database;
+    DatabaseReference roomRef;
+    DatabaseReference roomsRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        database = FirebaseDatabase.getInstance();
+        //get the player name and assign his room name to the player name
+        SharedPreferences preferences = getSharedPreferences("PREFS", 0);
+        playerName = preferences.getString("playerName", "");
+        roomName = playerName;
+
+        listView = findViewById(R.id.listView);
+        button = findViewById(R.id.button);
+        //all existing available rooms
+        roomsList = new ArrayList<>();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // create room and add yourself as player1
+                button.setText("CREATING ROOM");
+                button.setEnabled(false);
+                roomName = playerName;
+                roomRef = database.getReference("rooms/" + roomName + "/player1");
+                addRoomEventListener();
+                roomRef.setValue(playerName);
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //join an existing room and add yourself as player2
+                int position = 0;
+                roomName = roomsList.get(position);
+                roomRef = database.getReference("rooms/" + roomName + "/player2");
+                addRoomEventListener();
+                roomRef.setValue(playerName);
+            }
+        });
+
+        //show if new room is available
+        addRoomsEventListener();
+
+        }
+
+        private void addRoomEventListener(){
+        roomRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+               //join the room
+                button.setText("CREATE ROOM");
+                button.setEnabled(true);
+                Intent intent = new Intent(getApplicationContext(), MainActivity3.class);
+                intent.putExtra("roomName", roomName);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                //error
+                button.setText("CREATE ROOM");
+                button.setEnabled(true);
+                Toast.makeText(MainActivity2.this, "Error!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        }
+
+        private void addRoomsEventListener(){
+            roomsRef = database.getReference("rooms");
+            roomsRef.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    // show list of rooms
+                    roomsList.clear();
+                    Iterable<DataSnapshot> rooms = snapshot.getChildren();
+                    for(DataSnapshot snapshot1 : rooms){
+                       roomsList.add(snapshot1.getKey());
+
+                        ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity2.this,
+                                android.R.layout.simple_list_item_1, roomsList);
+                        listView.setAdapter(adapter);
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    //error - nothing
+                }
+            });
+        }
+}
